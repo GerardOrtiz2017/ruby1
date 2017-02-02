0d7a6ffd21fe22ff80bcd3ed42ea1881e1c176e0
Gerard Ortiz <fedgerardrortiz@lewisu.edu>
2017-02-02 17:30:34 +0000

    mif class commit

--- first.rb
+++ first.rb
@@ -2,8 +2,19 @@ print "Enter your name: "
 name = gets.chomp
 puts "Hello  #{name}"
 
+
 print "Enter the number of cups: "
 cups = gets.chomp
 cups = cups.to_i
 ounces = cups * 8
 puts "That is #{ounces}"
+
+print "Enter the temperature in Farenheit: "
+faren = gets.chomp.to_i
+cel = (faren - 32) * 5/9
+puts "#{faren} degrees Farenheit in is about #{cel} degrees Celsius"
+
+print "Enter your name: "
+name = gets.chomp
+new_name = name.reverse!.downcase.capitalize
+puts "Your name in reverse is #{new_name}"
---
+++ string_test.rb
@@ -0,0 +1,9 @@
+#playing with strings
+
+print "Enter your full name: "
+name = gets.chomp
+
+puts name.upcase
+puts name.downcase
+puts name.capitalize
+puts. name
